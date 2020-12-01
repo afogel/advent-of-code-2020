610e37afa435da95d76bff4a048fda3d592da26e
Alyssa Diaz <alyssa.diaz@stitchfix.com>
2020-12-01 10:34:04 -0600

    initial commit

---
+++ day_1/day_1.rb
@@ -0,0 +1,6 @@
+abort "Combo size required as arg" if ARGV[0].nil?
+combo_size = ARGV[0].to_i
+input = File.readlines('./input.txt').map(&:to_i)
+input.combination(combo_size).each do |combo|
+  puts combo.reduce(:*) if combo.reduce(:+) == 2020
+end
